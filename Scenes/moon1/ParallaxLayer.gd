c0fe2ef51317a8ad4831e676d4534015f597da1c
kukosek <kukys410@gmail.com>
2020-11-09 15:21:55 +0100

    Make new scene from moon background to make it reusable

--- Scenes/moon1/ParallaxLayer.gd
+++ Scenes/moon1/ParallaxLayer.gd
@@ -3,7 +3,7 @@ extends ParallaxLayer
 
 var last_screen_size = Vector2()
 func _process(delta):
-	var screen_size = get_viewport_rect().size * get_parent().get_parent().get_node("Player/KinematicBody2D/Camera2D").zoom
+	var screen_size = get_viewport_rect().size * get_tree().get_root().get_node("Node2D/Player/KinematicBody2D/Camera2D").zoom
 	if screen_size != last_screen_size:
 		var sprite_scale = 4
 		var sprite = get_children()[0]
