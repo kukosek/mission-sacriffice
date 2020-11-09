05475805363e7f821082c782c0a3ba359f1702d7
kukosek <kukys410@gmail.com>
2020-11-09 17:35:19 +0100

    change player sprite

--- Scenes/moon1/Sunlight.gd
+++ Scenes/moon1/Sunlight.gd
@@ -9,11 +9,18 @@ func _ready():
 	var tilemap = get_parent().get_node("TileMap")
 	tilemap_top_y = (8+light_cells_outreach)*tilemap.cell_size.y*tilemap.scale.y
 var last_screen_size = Vector2()
+var base_position_y
 func _process(delta):
 	var screen_size = get_viewport_rect().size * get_parent().get_node("Player/KinematicBody2D/Camera2D").zoom
 	if screen_size != last_screen_size:
 		var light_size = screen_size*1.2
 		scale = light_size/texture_size
-		position.y = (tilemap_top_y-light_size.y) + light_size.y / 2
-	position.x = get_parent().get_node("Player/KinematicBody2D").position.x
+		base_position_y = (tilemap_top_y-light_size.y) + light_size.y / 2
+	var playerpos = get_parent().get_node("Player/KinematicBody2D").position
+	position.x = playerpos.x
+	if playerpos.y < base_position_y:
+		print(playerpos.y)
+		position.y = playerpos.y
+	else:
+		position.y = base_position_y
 	last_screen_size = screen_size
