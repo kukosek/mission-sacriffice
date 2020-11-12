77d5a3375ae6fbc7cdcbe958a00c4ff9bdb39404
kukosek <kukys410@gmail.com>
2020-11-12 21:32:51 +0100

    Enemy shooting animation (doesnt actually fire)

--- Scenes/enemy/Enemy.gd
+++ Scenes/enemy/Enemy.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-var sprite
-
+onready var sprite = $AnimatedSprite
+onready var player_detection = $PlayerDetection
 var velocity = Vector2.ZERO
 export (int) var gravity = 1000
 export (int) var walk_speed = 150
@@ -11,7 +11,6 @@ var positions_count
 var pos_iter = 0
 
 func _ready():
-	sprite = $AnimatedSprite
 	var follow_positions_group_node = get_parent().get_node_or_null("FollowPositions")
 	if follow_positions_group_node == null:
 		positions = null
@@ -32,21 +31,25 @@ func set_veloc_by_target_point(target_pos):
 
 var standing_timer = 0
 func _physics_process(delta):
-	if positions != null:
-		var target = positions[pos_iter]
-		var target_pos = target.position
-		
-		
-		if velocity.x == 0:
-			standing_timer += delta
-		if (velocity.x > 0 and target_pos.x <= position.x) or (velocity.x < 0 and target_pos.x >= position.x):
-			velocity.x = 0
-		if standing_timer >= target.stay_time:
-			standing_timer = 0
-			pos_iter += 1
-			if pos_iter >= positions_count:
-				pos_iter = 0
-			set_veloc_by_target_point(positions[pos_iter].position)
+	if player_detection.detected:
+		if sprite.animation != "gunshot": sprite.play("gunshot")
+	else:
+		if positions != null:
+			var target = positions[pos_iter]
+			var target_pos = target.position
 			
+			
+			if velocity.x == 0:
+				standing_timer += delta
+			if (velocity.x > 0 and target_pos.x <= position.x) or (velocity.x < 0 and target_pos.x >= position.x):
+				velocity.x = 0
+				sprite.play("default")
+			if standing_timer >= target.stay_time:
+				standing_timer = 0
+				pos_iter += 1
+				if pos_iter >= positions_count:
+					pos_iter = 0
+				set_veloc_by_target_point(positions[pos_iter].position)
+				sprite.play("walking")
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2(0, -1))
