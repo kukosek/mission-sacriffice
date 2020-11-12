6e9b883713b02bf02b214976beb4ebf28678c65d
kukosek <kukys410@gmail.com>
2020-11-12 20:45:46 +0100

    PlayerDetection add 2nd playerdetector raycast

--- Scenes/enemy/PlayerDetection.gd
+++ Scenes/enemy/PlayerDetection.gd
@@ -6,6 +6,8 @@ onready var wall_detector_left = $WallDetectorLeft
 onready var wall_detector_right = $WallDetectorRight
 onready var player_detector_left = $PlayerDetectorLeft
 onready var player_detector_right = $PlayerDetectorRight
+onready var player_detector_left2 = $PlayerDetectorLeft2
+onready var player_detector_right2 = $PlayerDetectorRight2
 
 var detected = false
 var detected_position = null
@@ -16,20 +18,31 @@ func _ready():
 const cast_limit_offset = -15
 func _process(delta):
 	if wall_detector_right.is_colliding():
-		player_detector_right.cast_to.x = wall_detector_right.get_collision_point().x- global_position.x + cast_limit_offset
+		var castpoint = wall_detector_right.get_collision_point().x- global_position.x + cast_limit_offset
+		player_detector_right.cast_to.x = castpoint
+		player_detector_right2.cast_to.x = castpoint
 	else:
 		player_detector_right.cast_to.x = enemy_vision
+		player_detector_right2.cast_to.x = enemy_vision
 	if wall_detector_left.is_colliding():
-		
-		player_detector_left.cast_to.x = -(global_position.x - wall_detector_left.get_collision_point().x + cast_limit_offset)
+		var castpoint = -(global_position.x - wall_detector_left.get_collision_point().x + cast_limit_offset)
+		player_detector_left.cast_to.x = castpoint
+		player_detector_left2.cast_to.x = castpoint
 	else:
 		player_detector_left.cast_to.x = -enemy_vision
+		player_detector_left2.cast_to.x = -enemy_vision
 	if player_detector_left.is_colliding():
 		detected = true
 		detected_position = player_detector_left.get_collision_point()
+	elif player_detector_left2.is_colliding():
+		detected = true
+		detected_position = player_detector_left2.get_collision_point()
 	elif player_detector_right.is_colliding():
 		detected = true
 		detected_position = player_detector_right.get_collision_point()
+	elif player_detector_right2.is_colliding():
+		detected = true
+		detected_position = player_detector_right2.get_collision_point()
 	else:
 		detected = false
 		detected_position = null
