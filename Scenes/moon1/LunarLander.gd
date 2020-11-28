245fa363fc6c761669db0fcf158003f93f79af20
kukosek <kukys410@gmail.com>
2020-11-28 20:27:15 +0100

    Lunar lander scene with audio

--- Scenes/moon1/LunarLander.gd
+++ Scenes/moon1/LunarLander.gd
@@ -8,13 +8,20 @@ var player_in_lander = true
 var player
 var camera
 
+onready var fire_sprite = $FireSprite
+
 func _ready():
+	velocity.y = 2000
 	player = get_parent().get_node("Player/Player")
 	player.in_lander = true
 	camera = player.get_node("Camera2D")
 func _physics_process(delta):
-	velocity.y += gravity * delta
+	
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+	if position.y > -350 and velocity.y > 0:
+		if velocity.y > 200 and position.y < 5000:
+			velocity.y -= gravity * delta * 2.7
+		fire_sprite.play("land")
 	if player_in_lander:
 		player.position = position
 		player.position.y -= 100
@@ -23,3 +30,8 @@ func _physics_process(delta):
 			player_in_lander = false
 			camera.reset_offset()
 			camera.target_zoom = Vector2(1.0, 1.0)
+
+
+func _on_FireSprite_animation_finished():
+	if fire_sprite.animation == "land":
+		fire_sprite.visible = false
