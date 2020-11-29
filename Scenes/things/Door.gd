b332c038f5706591c3636d7bb2b37a81c218fe72
kukosek <kukys410@gmail.com>
2020-11-29 15:57:06 +0100

    bossroom door and background

--- Scenes/things/Door.gd
+++ Scenes/things/Door.gd
@@ -7,9 +7,12 @@ extends StaticBody2D
 export (int) var range_x = 200
 export (int) var range_y = 100
 export (bool) var show_hint = true
+export (String) var target_scene
+export (NodePath) var player_path = "../Player"
+
 
 onready var sprite = $AnimatedSprite
-onready var player = get_parent().get_node("Player/Player")
+onready var player = get_node(player_path).get_node("Player")
 onready var open_sfx = $OpenSFX
 onready var close_sfx = $CloseSFX
 
@@ -28,10 +31,10 @@ func toggleOpen():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$LabEntranceDetection.target_scene = target_scene
 
 func player_is_in_range():
-	var delta_pos = Vector2(abs(player.position.x - position.x), abs(player.position.y - position.y))
+	var delta_pos = Vector2(abs(player.global_position.x - global_position.x), abs(player.global_position.y - global_position.y))
 	return delta_pos.x < range_x and delta_pos.y < range_y
 
 func _input(event):
