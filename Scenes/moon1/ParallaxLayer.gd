bfe95239d8ecf09d938d5fc489ec66618d8e39b2
kukosek <kukys410@gmail.com>
2020-11-29 21:54:03 +0100

    Control panel, not working yho

--- Scenes/moon1/ParallaxLayer.gd
+++ Scenes/moon1/ParallaxLayer.gd
@@ -3,7 +3,7 @@ extends ParallaxLayer
 
 var last_screen_size = Vector2()
 func _process(_delta):
-	var screen_size = get_viewport_rect().size * get_tree().get_root().get_node("Moon/Player/Player/Camera2D").zoom
+	var screen_size = get_viewport_rect().size * get_tree().current_scene.get_node("Player/Player/Camera2D").zoom
 	if screen_size != last_screen_size:
 		var sprite_scale = 4
 		var sprite = get_children()[0]
