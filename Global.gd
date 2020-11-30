e3045718a0e9d77a3540d8997b71f8a0b901c8c8
kukosek <kukys410@gmail.com>
2020-11-30 18:59:54 +0100

    Voice areas system

--- Global.gd
+++ Global.gd
@@ -8,3 +8,5 @@ var counting_down = false
 var explode_time_left = 20
 var exploding = false
 var lab_destroy = false
+
+var voice_areas_visited_names = []
---
+++ Scenes/things/VoiceArea.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+onready var collision = $CollisionShape2D
+export (AudioStreamOGGVorbis) var stream = load("res://Scenes/things/cia_voice/go ageah and check it.ogg")
+var player
+func _ready():
+	player = AudioStreamPlayer2D.new()
+	player.stream = stream
+	add_child(player)
+	player.global_position = collision.global_position
+	connect("body_entered", self, "player_entered")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func player_entered(body):
+	if not name in Global.voice_areas_visited_names:
+		player.play()
+		Global.voice_areas_visited_names.append(name)
