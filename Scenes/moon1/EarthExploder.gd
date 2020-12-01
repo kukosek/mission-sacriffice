51f3e6b9e02f81b549ee315bf56a0ad533712e55
kukosek <kukys410@gmail.com>
2020-12-01 19:22:44 +0100

    win lose

--- Scenes/moon1/EarthExploder.gd
+++ Scenes/moon1/EarthExploder.gd
@@ -9,11 +9,13 @@ export (float) var raninterval_max_s = 2.5
 export (float) var explosion_scale = 1
 export (NodePath) var player_path = null
 var player
+var end_screen
 func _ready():
 	if player_path != null:
 		player = get_node(player_path).get_node("Player")
 	else:
 		player = get_node("../../../Player/Player")
+	end_screen = player.get_node("HUD/EndScreen")
 onready var EXPLOSION = load("res://Scenes/things/explosions/Explosion.tscn")
 var rng = RandomNumberGenerator.new()
 
@@ -31,6 +33,8 @@ func spawn_explosion():
 	if pos_x_min != 800:
 		player.damage(1)
 var next_explosion_remaining_counter = 0
+var show_lose_remaining_counter = 2.0
+var showed_lose = false
 func _process(delta):
 	if exploding:
 		if next_explosion_remaining_counter <= 0:
@@ -39,3 +43,9 @@ func _process(delta):
 			next_explosion_remaining_counter = rng.randf_range(raninterval_min_s, raninterval_max_s)
 		else:
 			next_explosion_remaining_counter -= delta
+		if not showed_lose and not Global.lab_destroy:
+			if show_lose_remaining_counter <= 0:
+				end_screen.lose()
+				showed_lose = true
+			else:
+				show_lose_remaining_counter -= delta
